05804b67d060aca2bf93072bf6b04b02812d6fce
Hkushimo <Hkushimo@users.noreply.github.com>
2026-09-22 00:00:10 -0400

    Add mobile admin panel spacing

--- docs/_next/static/chunks/00anrc23__87a.js
+++ docs/_next/static/chunks/00anrc23__87a.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/2t0oas28t-ifw.js",
   "static/chunks/0ra7zydpfknjg.js",
   "static/chunks/27hfarqwxn3-q.js",
-  "static/chunks/2ml2dvxqbubyg.css",
-  "static/chunks/turbopack-1wr310tdc5wkm.js"
+  "static/chunks/0op_skuecargl.css",
+  "static/chunks/turbopack-1h3ieckk3726z.js"
 ])
